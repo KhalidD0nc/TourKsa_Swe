4edbbb8c052c4560c27fe2000498de9b92c414ed
Khalid <khalidald4z@gmail.com>
2025-01-26 22:08:56 +0300

    Initial Commit

---
+++ TourKsa_Swe/TourKsa_SweApp.swift
@@ -0,0 +1,17 @@
+//
+//  TourKsa_SweApp.swift
+//  TourKsa_Swe
+//
+//  Created by Khalid R on 26/07/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct TourKsa_SweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
