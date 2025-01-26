f01b1c091193c2226399a3e65373d365d27727e5
Khalid <khalidald4z@gmail.com>
2025-01-26 23:34:43 +0300

    Add test

---
+++ TourKsa_Swe/Test/TourKsa_SweTests.swift
@@ -0,0 +1,16 @@
+//
+//  TourKsa_SweTests.swift
+//  TourKsa_Swe
+//
+//  Created by Khalid R on 26/07/1446 AH.
+//
+
+import XCTest
+
+final class TourKsa_SweTests: XCTestCase {
+
+    func testExample() {
+           XCTAssertTrue(true, "This is a placeholder test that always passes.")
+       }
+
+}
